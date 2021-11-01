61b9c3f2af33cbad663aea2c6ed6ae3a551ac0bf
andrewKievUA <andrew1990mk@gmail.com>
2021-11-01 19:02:08 +0200

    redux optimize createStructuredSelector  icon component

--- src/App.js
+++ src/App.js
@@ -7,21 +7,19 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 import {connect} from "react-redux";
+
 import {setCurrentUser} from "./redux/user/user.actions";
+import {selectCurrentUser} from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 
 class App extends Component {
-
     unsubscribeFromAuth = null
-
     componentDidMount() {
         const {setCurrentUser}=this.props
-
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth){
             const userRef = await createUserProfileDocument(userAuth)
-
-
                  userRef.onSnapshot(snapshot => {
                      setCurrentUser({
                          currentUser:{
@@ -29,12 +27,9 @@ class App extends Component {
                              ...snapshot.data()
                          }
                      })
-
                  })
             }
             setCurrentUser( userAuth)
-
-
         })
     }
 
@@ -43,11 +38,22 @@ class App extends Component {
     }
 
     render() {
+        let fff = (<div> "null" </div>)
+        if (this.props.currentUser){
+            if (this.props.currentUser.currentUser){
+                fff = (<div> {this.props.currentUser.currentUser.displayName} </div>)
+            }
 
+        }
 
         return (<>
-
-
+                {fff}
+                {this.props.currentUser?
+                this.props.currentUser.currentUser?
+                this.props.currentUser.currentUser.displayName:
+                "null":
+                "null"
+            }
                 <Header/>
                 <Switch>
                     <Route exact path="/" component={HomePage}/>
@@ -62,9 +68,8 @@ const mapDispatchToProps =dispatch=>({
     setCurrentUser:user => dispatch(setCurrentUser(user))
 })
 
-const mapStateToProps= ({user})=>({
-    currentUser: user.currentUser
-
+const mapStateToProps= createStructuredSelector({
+    currentUser:selectCurrentUser
 })
 export default connect(mapStateToProps,mapDispatchToProps) (App);
 
--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -5,11 +5,9 @@ import {connect} from "react-redux";
 import CartItem from "../cart-item/cart-item.component";
 import {toggleCartHidden } from "../../redux/cart/cart.actions";
 import {selectCartItems} from "../../redux/cart/cart.selectors";
-
-
+import {createStructuredSelector} from 'reselect'
 
 const CartDropdown = ({cartItems,toggleCartHiddenAA}) => {
-
     // console.log(toggleCartHiddenAA, "toglecard hidddden")
     // console.log(cartItems,"cartItems")
     return(
@@ -22,14 +20,12 @@ const CartDropdown = ({cartItems,toggleCartHiddenAA}) => {
         <CustomButton > GO TO CHECKOUT</CustomButton>
     </div>)
 }
-const mapStateToProps=(state)=>({
-cartItems:selectCartItems(state)
+const mapStateToProps=createStructuredSelector({
+cartItems:selectCartItems
 })
-
 
 const mapDispatchToProps = dispatch=>({
     toggleCartHiddenAA:()=>dispatch(toggleCartHidden())
 })
-
 
 export default connect(mapStateToProps,mapDispatchToProps)(CartDropdown)
