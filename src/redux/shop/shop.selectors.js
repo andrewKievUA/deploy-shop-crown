2e87df942d169ac8f49cc940b7d6241c2d0e6abe
andrewKievUA <andrew1990mk@gmail.com>
2021-11-09 09:44:37 +0200

    add map id overwiew of the page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,6 +1,15 @@
 
 import { createSelector } from 'reselect';
 
+const CollectionIdMap = {
+    hats:1,
+    jackets:3,
+    mens:5,
+    sneakers:2,
+    womens: 4
+
+}
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,5 +25,12 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
-        collections => collections[collectionUrlParam]
+        collections => collections.find(collection =>collection.id=== CollectionIdMap[collectionUrlParam])
     );
+
+
+// export const selectCollection = collectionUrlParam =>
+//     createSelector(
+//         [selectCollections],
+//         collections => collections[collectionUrlParam]
+//     );
