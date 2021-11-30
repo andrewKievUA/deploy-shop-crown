28a1c28eae143e0d5161094fc519fcb03003a2a3
andrewKievUA <andrew1990mk@gmail.com>
2021-11-30 11:42:15 +0200

    configurated for vercel package build

---
+++ build/precache-manifest.852fd67ee16678bb91dcd6ffc61c9ed2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aed4f6f59cdf2cf880b2",
+    "url": "/static/css/main.a8b2be1f.chunk.css"
+  },
+  {
+    "revision": "aed4f6f59cdf2cf880b2",
+    "url": "/static/js/main.017453f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b74e9aa9f7326b33ca39",
+    "url": "/static/js/2.5cc8cab4.chunk.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "63ab0409907007de7f580d1dcfc54bdd",
+    "url": "/index.html"
+  }
+];
